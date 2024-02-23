98cc7d0f0ec21c286ecdf42b0c144ea195e81418
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2024-02-23 16:42:15 +0200

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -5,6 +5,7 @@ import surahs from "@/components/home/DataQuranSurah";
 import NextSurah from "@/components/Chapter/nextSurah";
 import Footer from "@/components/Footer";
 import GotoAyah from "@/components/Chapter/gotoAyah";
+import type { Metadata } from 'next'
 //import { useRouter } from "next/router";
 
 // ... (your existing imports)
@@ -14,6 +15,11 @@ export async function generateStaticParams() {
     id: item.id.toString(),
   }));
 }*/
+export const metadata: Metadata = {
+  title: 'AerabUlQuran',
+  description: 'Embark on a captivating journey through the linguistic labyrinth of the Holy Quran with "aerabUlQuran." Delve into the intricate world of Nahw analysis, where every word is a revelation. Uncover the beauty of Quranic syntax and Arabic grammar, as you explore the profound depths of linguistic structure within the sacred text.',
+  keywords: ['Quranic grammar', 'Nahw analysis', 'Arabic grammar', 'Quranic linguistics', 'Quranic syntax', 'Morphological analysis', 'Quranic language', 'Arabic syntax', 'Nahw principles', 'Linguistic analysis', 'Morphological breakdown', 'Arabic morphology', 'Quranic grammar guide', 'Nahw concepts', 'Quranic linguistic structure', 'Nahw tutorials', 'Arabic language analysis', 'Quranic grammar study', 'Linguistic principles', 'Quranic grammar resources']
+}
 async function getData(id) {
   const items = data.filter((item) => item.surah.toString() === id);
   return items;
