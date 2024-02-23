5e55346d4000a02a50d6384c514c4cfda53fee81
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2024-02-23 16:34:48 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import Hero from "@/components/home/Hero";
 import Navbar from "@/components/Navbar";
 import SurahList from "@/components/home/SurahList";
 import Footer from "@/components/Footer";
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
+  title: 'Explore the Linguistic Depths of the Holy Quran with aerabUlQuran',
+  description: 'Embark on a captivating journey through the linguistic labyrinth of the Holy Quran with "aerabUlQuran." Delve into the intricate world of Nahw analysis, where every word is a revelation. Uncover the beauty of Quranic syntax and Arabic grammar, as you explore the profound depths of linguistic structure within the sacred text.',
+}
 export default function Home() {
   return (
     <main>
