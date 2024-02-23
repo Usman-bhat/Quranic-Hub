99de9dd55b8f7040b9dc2c366afd114931e21b02
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2024-02-23 19:43:48 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,9 +9,8 @@ const AboutUs = () => {
       <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-b from-blue-500 via-gray-400 to-orange-500">
         <h1 className="text-5xl font-bold text-gray-800 mb-10">About Us</h1>
         <p className="text-gray-700 text-center max-w-2xl">
-          BAL About Welcome to QuranicHub, the online haven for seekers of
-          Islamic wisdom and the profound teachings of the Quran. I am Mohammad
-          Usman Bhat, the mind and heart behind this endeavor, and it is my
+          Welcome to QuranicHub, the online haven for seekers of
+          Islamic wisdom and the profound teachings of the Quran.it is our
           privilege to guide you through the diverse realms of Islamic
           knowledge. At QuranicHub, our mission is rooted in a deep commitment
           to provide authentic and enriching content. From the melodious
@@ -23,7 +22,7 @@ const AboutUs = () => {
           Bayans from scholars like Dr. Israr Ahmad resonate within our digital
           walls, offering profound insights and guidance for spiritual
           enrichment. But our mission extends beyond the Quranic realm. In a
-          tribute to the great philosopher-poet Allama Iqbal, I have curated a
+          tribute to the great philosopher-poet Allama Iqbal, We have curated a
           comprehensive collection of his works, including both Urdu and Persian
           masterpieces. This online archive aims to disseminate Iqbal's profound
           thoughts and poetic brilliance across the vast expanse of the
@@ -45,7 +44,7 @@ const AboutUs = () => {
         <p className="text-gray-700 text-center max-w-2xl mt-5 arabicFont">
           "إعراب القرآن" (I'rab al-Qur'an) delves into the grammatical
           intricacies of the Quranic Arabic language, offering profound insights
-          into its structure and meaning. Authored by [Author's Name], this
+          into its structure and meaning. Authored by Molana MahiudDeen Darwash, this
           comprehensive guide provides a meticulous analysis of Quranic verses,
           aiding readers in understanding the linguistic nuances and syntactical
           arrangements within the sacred text. With clear explanations and
