07715ec9bea7ddc9e747da6bb9c54be8489923f9
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2024-02-23 19:28:07 +0200

    Update SurahCard.tsx

--- components/Chapter/SurahCard.tsx
+++ components/Chapter/SurahCard.tsx
@@ -9,7 +9,7 @@ const SurahCard = ({ ayah, aerab, surah, size }) => {
 
   return (
     <div className="flex flex-col rounded-lg items-center p-4 m-4 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800 text-right">
-      <p className="bg-gray-700 rounded-lg p-6 text-[40px] font-semibold quranFont mb-4 rtl">
+      <p className="bg-gray-700 rounded-lg p-6 text-[25px] font-semibold quranFont mb-4 rtl">
         {ayah}
       </p>
       <hr className="  h-px my-8 bg-gray-800 dark:bg-gray-300" />
