afd26b374cf931c4a664abb5cb4feec535291cdf
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2024-02-23 19:18:06 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aerab Ul Quran",
   description: "Embark on a captivating journey through the linguistic labyrinth of the Holy Quran with aerabUlQuran. Delve into the intricate world of Nahw analysis, where every word is a revelation. Uncover the beauty of Quranic syntax and Arabic grammar, as you explore the profound depths of linguistic structure within the sacred text.",
-  generator: "QuranicHub",
+  
   manifest: "/manifest.json",
   keywords: ['Quranic grammar', 'Nahw analysis', 'Arabic grammar', 'Quranic linguistics', 'Quranic syntax', 'Morphological analysis', 'Quranic language', 'Arabic syntax', 'Nahw principles', 'Linguistic analysis', 'Morphological breakdown', 'Arabic morphology', 'Quranic grammar guide', 'Nahw concepts', 'Quranic linguistic structure', 'Nahw tutorials', 'Arabic language analysis', 'Quranic grammar study', 'Linguistic principles', 'Quranic grammar resources'],
  
@@ -20,12 +20,7 @@ export const metadata: Metadata = {
       url: "https://github.com/HASHIM-HAMEEM",
     },
   ],
-  icons: [
-    { rel: "apple-touch-icon", url: "./apple-touch-icon.png" },
-    { rel: "icon", url: "./icon-32x32.png" },
-    { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-    { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" }
-  ],
+ 
 };
 
 
